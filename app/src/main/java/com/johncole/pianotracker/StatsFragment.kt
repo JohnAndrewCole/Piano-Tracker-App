52c41eb1bfe4e5c4109ad36cbc9bc3badf07d73b
John <jo.andrew.cole@gmail.com>
2020-07-22 11:35:17 +0200

    Feature #28 added

--- app/src/main/java/com/johncole/pianotracker/StatsFragment.kt
+++ app/src/main/java/com/johncole/pianotracker/StatsFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.github.mikephil.charting.components.XAxis.XAxisPosition
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
@@ -34,12 +35,32 @@ class StatsFragment : Fragment() {
         val lineChart = binding.overTimeLineChart
 
         val values: ArrayList<Entry> = ArrayList()
-        values.add(Entry(1F, 50F))
-        values.add(Entry(2F, 100F))
+        values.add(Entry(1F, 1.5F))
+        values.add(Entry(1.4F, 2F))
+        values.add(Entry(2F, 1F))
 
         val dataSet = LineDataSet(values, "ThisIsALabel") // add entries to data set
         dataSet.color = Color.DKGRAY
         dataSet.valueTextColor = Color.RED
+        dataSet.valueTextSize = 12F
+
+        lineChart.legend.isEnabled = false
+        lineChart.axisRight.isEnabled = false
+        lineChart.description.text = ""
+
+        val xAxis = lineChart.xAxis
+        xAxis.position = XAxisPosition.BOTTOM
+        xAxis.setDrawLabels(true)
+        xAxis.axisLineColor = Color.BLUE
+        // TODO: Use this to set dates
+        // xAxis.valueFormatter = MyValueFormatter()
+
+        val yAxis = lineChart.axisLeft
+        yAxis.axisLineColor = Color.BLUE
+        yAxis.axisMinimum = 0f // start at zero
+        yAxis.axisMaximum = 6f // the axis maximum is 100
+        yAxis.granularity = 0.5f // interval 1
+
 
         val lineData = LineData(dataSet)
         lineChart.data = lineData
