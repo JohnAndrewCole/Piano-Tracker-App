44821b772a974c756dc26b8d244e661489fc42b8
John <jo.andrew.cole@gmail.com>
2020-08-25 11:19:23 +0200

    Added reference for timer code. Set a package name for debug builds.

--- app/src/main/java/com/johncole/pianotracker/utilities/Timer.kt
+++ app/src/main/java/com/johncole/pianotracker/utilities/Timer.kt
@@ -1,5 +1,11 @@
 package com.johncole.pianotracker.utilities
 
+/**
+ * I used the code found on this page:
+ * https://github.com/cgoldberg/netplot/blob/0cdac581460f70a3d39929ffb5a2635a9eb16320/src/Stopwatch.java
+ * to make this class.
+ */
+
 class Timer {
     private var startTime: Long = 0
     private var stopTime: Long = 0
